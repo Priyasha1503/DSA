e03a3296794e0103df147cf069515902074c4ac6
Priyasha1503 <priyashadevi@gmail.com>
2025-05-19 12:09:30 +0530

    Time: 11 ms (59.03%) | Memory: 18 MB (19.72%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+
+        res=[]
+        def helper(ind,target,arr,candidates,n):
+            if target<0:
+                return
+            if ind==n:
+                if target==0:
+    
+                    res.append(arr[:])
+                    return
+                else:
+                    return
+            if candidates[ind]<=target:
+                arr.append(candidates[ind])
+                helper(ind,target-candidates[ind],arr,candidates,n)
+                arr.pop()
+            helper(ind+1,target,arr,candidates,n)
+        
+        helper(0,target,[],candidates,len(candidates))
+        print(res)
+        return res
+
+
+
