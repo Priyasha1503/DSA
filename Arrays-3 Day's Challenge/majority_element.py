5a9205ee2b6cb6048a22e87744a7aa7f94e42f5b
Priyasha Sabbavarapu <priyashadevi@gmail.com>
2025-07-30 22:13:41 +0530

    Create majority_element.py

---
+++ Arrays-3 Day's Challenge/majority_element.py
@@ -0,0 +1,17 @@
+
+#https://leetcode.com/problems/majority-element/description/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #BRUTEFORCE - Taking a counter from collections,so that I could know the frequency, and compare the frequencies  -- but this couldnt be solved in o(1) space comp
+        ###BOYER MOORE ALGORITHM
+        candidate=None
+        count=0
+        for i in nums:
+            if count==0:
+                candidate=i
+            if candidate==i:
+                count+=1
+            else:
+                count-=1
+        return candidate
