f10a27c7c722e668254c8849c81ea06d891d1141
Priyasha Sabbavarapu <priyashadevi@gmail.com>
2025-09-22 22:11:51 +0530

    Create check_array_sorted_and_rotated.py

---
+++ Arrays/Arrays_week/check_array_sorted_and_rotated.py
@@ -0,0 +1,34 @@
+
+#https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        #BRUTEFORCE IN PYTHON
+        '''for i in range(0,len(nums)):
+            lst=nums[i:]+nums[:i]
+            flag=True
+            for i in range(0,len(lst)-1):
+                if lst[i]>lst[i+1]:
+                    flag=False
+                    break
+            if flag:
+                return True
+        return False'''
+        if len(nums)==1: return True
+        cnt=1
+        n=len(nums)
+        for i in range(1,2*n):
+            if nums[(i-1)%n]<=nums[i%n]:
+                cnt+=1
+            else:
+                cnt=1
+            if cnt==n:
+                return True
+        
+        return False
+
+
+                
+
+
+
+                
