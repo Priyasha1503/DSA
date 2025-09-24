d355ad0d553a65c3dc89d00cd6169242e4d406a4
Priyasha Sabbavarapu <priyashadevi@gmail.com>
2025-09-24 21:33:56 +0530

    Create rotate_image_90deg.py

---
+++ Arrays/Arrays_week/rotate_image_90deg.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/rotate-image/
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(0,len(matrix)-1):
+            for j in range(i+1,len(matrix[0])):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        for i in matrix:
+            i.reverse()
+        
