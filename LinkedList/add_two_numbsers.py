ba482093c9aa024ec803f07dc3a7184a966dd569
Priyasha Sabbavarapu <priyashadevi@gmail.com>
2025-11-16 11:23:08 +0530

    Refactor addTwoNumbers to sum linked list values
    Refactor addTwoNumbers method to convert linked list to string, sum the values, and reconstruct the linked list from the sum.

--- LinkedList/add_two_numbsers.py
+++ LinkedList/add_two_numbsers.py
@@ -1,11 +1,46 @@
-#https://leetcode.com/problems/add-two-numbers/
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        str1,str2="",""
+
+        while l1 is not None:
+            str1=str1+str(l1.val)
+            l1=l1.next
+        
+        while l2 is not None:
+            str2=str2+str(l2.val)
+            l2=l2.next
+        
+        num1=int(str1[::-1])
+        num2=int(str2[::-1])
+        sums=num1+num2
+        sums=str(sums)
+        sums=sums[::-1]
+        dummy=ListNode()
+        curr=dummy
+        x=0 #for index of sums
+        while curr and x<len(sums):
+            curr.next=ListNode(int(sums[x]))
+            curr=curr.next
+            x+=1
+
+        return dummy.next
+
+
+
+
+#more better 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+'''class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         num1,num2="",""
         dummy=ListNode()
@@ -24,10 +59,11 @@ class Solution:
             temp3.next=ListNode(int(sums[ind]))
             temp3=temp3.next
             ind-=1
-        return dummy.next
+        return dummy.next '''
 
 
 
 
             
         
+
