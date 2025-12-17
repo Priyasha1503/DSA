921a025340ffb33af661129e953e34d2ad72c89c
Priyasha Sabbavarapu <priyashadevi@gmail.com>
2025-12-17 23:08:33 +0530

    Add merge function for merging sorted arrays
    Implement merge function to combine two sorted arrays in-place.

---
+++ Arrays/Merge_sorted_arrays.py
@@ -0,0 +1,27 @@
+
+#https://leetcode.com/problems/merge-sorted-array/
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        x=m-1
+        y=n-1
+        z=m+n-1
+        while x>=0 and y>=0:
+            if nums1[x]>nums2[y]:
+                nums1[z]=nums1[x]
+                x-=1
+                z-=1
+            else:
+                nums1[z]=nums2[y]
+                y-=1
+                z-=1
+        while y>=0:   #REMAINING IF THERE ARE ELEMENTS IN ARRAY2 THEN ADDING THEM AS WELL
+            nums1[z]=nums2[y]
+            z-=1
+            y-=1
+
+
